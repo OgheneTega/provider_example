13d06a24dd0a6008176e895ec8c7786577439c18
Rotibi Adedeji <dayjeerow7@gmail.com>
2022-04-22 08:37:39 +0100

    add cart functionality

---
+++ lib/models/cart.dart
@@ -0,0 +1,50 @@
+import 'package:collection/collection.dart';
+import 'cart_item.dart';
+
+class Cart {
+  List<CartItem>? items = [];
+
+  Cart({this.items});
+
+  get sum {
+    if (hasItems) {
+      return items!.map((item) => item.total).reduce((a, b) => a + b);
+    }
+    return 0;
+  }
+
+  get itemCount {
+    if (hasItems) {
+      return items!.map((item) => item.quantity).reduce((a, b) => a + b);
+    }
+    return 0;
+  }
+
+  bool get hasItems {
+    return items!.isNotEmpty;
+  }
+
+  bool add(CartItem item) {
+    CartItem? it = items!.firstWhereOrNull((l) => l.food.id == item.food.id);
+
+    if (it != null) {
+      it.quantity++;
+    } else {
+      items!.insert(0, item);
+    }
+
+    return true;
+  }
+
+  bool remove(CartItem item) {
+    items!.removeWhere((it) => it.food.id == item.food.id);
+
+    return true;
+  }
+
+  bool empty() {
+    items = [];
+
+    return true;
+  }
+}
--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -1,3 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:collection/collection.dart';
 
-class CartProvider extends ChangeNotifier {}
+import '../models/cart.dart';
+import '../models/cart_item.dart';
+import '../models/food.dart';
+
+class CartProvider extends ChangeNotifier {
+  List<Food> foods = [
+    Food(
+      id: 101,
+      name: "Akara, Pap, Milk And Sugar",
+      image: "akara.png",
+      amount: 30.00,
+      color: Colors.orangeAccent,
+    ),
+    Food(
+      id: 102,
+      name: "Boiled Yam, Chicken Stew (Chicken Cooked In Stew)",
+      image: "yam.png",
+      amount: 10.00,
+      color: Colors.redAccent,
+    ),
+    Food(
+      id: 103,
+      name: "Plain Spaghetti, Croaker Fish Stew, Fried Plantain Side",
+      image: "noodles.png",
+      amount: 15.00,
+      color: Colors.greenAccent,
+    ),
+  ];
+
+  final Cart _cart = Cart(items: []);
+  Cart get cart => _cart;
+
+  removeItemFromCart({required CartItem item}) {
+    CartItem? it = cart.items!.firstWhereOrNull(
+      (el) => el.food.id == item.food.id,
+    );
+
+    if (it != null) {
+      cart.items!.remove(it);
+      notifyListeners();
+    }
+  }
+
+  increaseItemInCart({required dynamic item}) {
+    if (item is CartItem) {
+      cart.add(item);
+    } else {
+      cart.add(CartItem(food: item));
+    }
+    notifyListeners();
+  }
+
+  decreaseItemInCart({required CartItem item}) {
+    CartItem? it = cart.items!.firstWhereOrNull(
+      (el) => el.food.id == item.food.id,
+    );
+
+    if (it != null) {
+      if (it.quantity > 1) {
+        --it.quantity;
+      } else {
+        cart.items!.remove(it);
+      }
+      notifyListeners();
+    }
+  }
+}
